cd7220435b15377a89b1a0cbf05bcbed5ac2f96a
terremou <terrence_mou@foxmail.com>
2021-12-28 16:42:04 +0800

    deploy

---
+++ precache-manifest.69fce13e6d3d56f1a5952ee4ab1420dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e665db4e12600dcc6424cdf94f742fc",
+    "url": "/Simpney-react-website/index.html"
+  },
+  {
+    "revision": "2ac0171c9b7e7c3f80b6",
+    "url": "/Simpney-react-website/static/css/main.2839d362.chunk.css"
+  },
+  {
+    "revision": "7333fa4031f069aa0a6a",
+    "url": "/Simpney-react-website/static/js/2.484f2290.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/Simpney-react-website/static/js/2.484f2290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac0171c9b7e7c3f80b6",
+    "url": "/Simpney-react-website/static/js/main.babd132a.chunk.js"
+  },
+  {
+    "revision": "4f8e3225cb8666276ae6",
+    "url": "/Simpney-react-website/static/js/runtime-main.48574dad.js"
+  }
+]);
